d679c5dea998dedbb122ea165f535ef0ef5120ba
Fernandez Ludovic <lfernandez.dev@gmail.com>
2016-05-29 04:17:02 +0200

    test: more tests

--- test/app.js
+++ test/app.js
@@ -43,18 +43,18 @@ describe('generator-atom-package-plus:app', function() {
   describe('running on new project', function() {
 
     before(function() {
+      this.answers = {
+        name: 'atom-generator-test',
+        description: 'My package',
+        authorName: 'Me',
+        authorEmail: 'me@me.com',
+        authorUrl: 'http://fakeurl.com',
+        keywords: ['foo', 'bar'],
+        features: ['grammars', 'keymaps', 'menus', 'settings', 'snippets', 'styles'],
+        githubAccount: 'fake'
+      };
       return helpers.run(path.join(__dirname, '../generators/app'))
-        .withPrompts({
-          name: 'atom-generator-test',
-          description: 'My package',
-          authorName: 'Me',
-          authorEmail: 'me@me.com',
-          authorUrl: 'http://fakeurl.com',
-          keywords: ['foo', 'bar'],
-          features: ['grammars', 'keymaps', 'menus', 'settings', 'snippets', 'styles'],
-          // features: ['nothing'],
-          githubAccount: 'fake'
-        })
+        .withPrompts(this.answers)
         .toPromise();
     });
 
@@ -81,6 +81,87 @@ describe('generator-atom-package-plus:app', function() {
       ]);
     });
 
+    it('creates package.json', function() {
+      assert.file('package.json');
+      assert.jsonFileContent('package.json', {
+        name: 'generator-test',
+        version: '0.0.0',
+        description: this.answers.description,
+        homepage: this.answers.homepage,
+        engines: {
+          atom: ">=1.0.0 <2.0.0"
+        },
+        author: {
+          name: this.answers.authorName,
+          email: this.answers.authorEmail,
+          url: this.answers.authorUrl
+        },
+        keywords: this.answers.keywords,
+        main: 'lib/main'
+      });
+    });
+
+    it('creates and fill contents in PULL_REQUEST_TEMPLATE.md', function() {
+      assert.file('.github/PULL_REQUEST_TEMPLATE.md');
+      assert.fileContent('.github/PULL_REQUEST_TEMPLATE.md', '- Read the [contributing guide](https://github.com/fake/atom-generator-test/blob/master/.github/CONTRIBUTING.md).');
+    });
+
+  });
+
+  describe('running on new project without features', function() {
+
+    before(function() {
+      this.answers = {
+        name: 'atom-generator-test',
+        description: 'My package',
+        authorName: 'Me',
+        authorEmail: 'me@me.com',
+        authorUrl: 'http://fakeurl.com',
+        keywords: ['foo', 'bar'],
+        features: [],
+        githubAccount: 'fake'
+      }
+      return helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts(this.answers)
+        .toPromise();
+    });
+
+    it('creates files', function() {
+      assert.file([
+        'readme.md',
+        '.coffeelintignore',
+        '.gitignore',
+        '.travis.yml',
+        'appveyor.yml',
+        'coffeelint.json',
+        'package.json',
+        '.github/CONTRIBUTING.md',
+        '.github/ISSUE_TEMPLATE.md',
+        '.github/PULL_REQUEST_TEMPLATE.md',
+        'lib/main.coffee',
+        'spec/package-spec.coffee',
+      ]);
+    });
+
+    it('creates package.json', function() {
+      assert.file('package.json');
+      assert.jsonFileContent('package.json', {
+        name: 'generator-test',
+        version: '0.0.0',
+        description: this.answers.description,
+        homepage: this.answers.homepage,
+        engines: {
+          atom: ">=1.0.0 <2.0.0"
+        },
+        author: {
+          name: this.answers.authorName,
+          email: this.answers.authorEmail,
+          url: this.answers.authorUrl
+        },
+        keywords: this.answers.keywords,
+        main: 'lib/main'
+      });
+    });
   });
 
 });
